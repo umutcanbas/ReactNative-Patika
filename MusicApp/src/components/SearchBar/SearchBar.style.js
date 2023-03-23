4d8c8adcfeb6c1584ed3963abc04084a05701884
Umutcan BAŞ <umutbasinfo@gmail.com>
2023-03-24 01:20:59 +0300

    Search bar styles component added

---
+++ MusicApp/src/components/SearchBar/SearchBar.style.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container:{
+        backgroundColor:"#d3d3d3",
+        padding:1,
+        margin:5,
+        borderRadius:5,
+        height:40,
+    },
+})
