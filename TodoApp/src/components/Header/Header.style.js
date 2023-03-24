4fbbefaaad61060876876c6b1ab79bef7991bf4f
Umutcan BAŞ <umutbasinfo@gmail.com>
2023-03-24 15:41:32 +0300

    Header style component added

---
+++ TodoApp/src/components/Header/Header.style.js
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native';
+
+export default styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+
+  text: {
+    fontSize: 35,
+    marginTop: 10,
+    color: 'yellow',
+  },
+  count: {
+    fontSize: 35,
+    color: 'yellow',
+    margin: 10,
+  },
+});
